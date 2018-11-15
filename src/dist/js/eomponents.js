fa0043edf128030910397059de4e730c127ab0be
Eoussama <oussama_gtacj@live.fr>
2018-11-15 18:25:04 +0000

    Added textboxses.

--- src/dist/js/eomponents.js
+++ src/dist/js/eomponents.js
@@ -1,60 +1,33 @@
-// Progress Bar
-function setProgressBar(progressBar, value = 0) {
-	value = value > 100 ? 100 : value < 0 ? 0 : value;
-	
-	progressBar.style.width = `${value}%`;
-	progressBar.querySelector('span.progressValue').textContent = value;
-}
-
-function incProgressBar(progressBar, value) {
-	if(progressBar.querySelector('span.progressValue').textContent === '')
-		progressBar.querySelector('span.progressValue').textContent = 0;
-	
-	setProgressBar(progressBar, Number(progressBar.querySelector('span.progressValue').textContent) + Number(value));
-}
-
-function decProgressBar(progressBar, value) {
-	if(progressBar.querySelector('span.progressValue').textContent === '')
-		progressBar.querySelector('span.progressValue').textContent = 0;
-	
-	setProgressBar(progressBar, Number(progressBar.querySelector('span.progressValue').textContent) - Number(value));
-}
-
-// Dropdowns
 window.addEventListener('load', () => {
-	const __dropdowns = document.querySelectorAll('div.dropdown');
+	// Textboxes
+	(function () {
+		const textboxes = document.querySelectorAll('div.textbox');
 
-	__dropdowns.forEach((__dropdown) => {
-		let
-			__trigger = __dropdown.querySelector('.btn'),
-			__dropable = __dropdown.querySelector('div.btn-piano');
+		textboxes.forEach(textbox => {
+			const input = textbox.querySelector('[type="text"]');
+			
+			// Resetting the placeholder of the input.
+			input.placeholder = '';
 
-		__dropable.style.display = 'none';
-		__trigger.addEventListener('click', () => {
-			if(__dropable.style.display === 'none') {
-				__dropable.style.top = (__trigger.offsetTop + __trigger.offsetHeight).toString() + 'px';
-				__dropable.style.left = __trigger.offsetLeft + 'px';
-				__dropable.style.display = 'inline';
-				__trigger.classList.add('dropped');
-			} else {
-				__dropable.style.display = 'none';
-				__trigger.classList.remove('dropped');
-			}
-		});
-	});
+			input.addEventListener('focus', () => {
+				if (!textbox.classList.contains('focused') && document.activeElement == input && !textbox.classList.contains('disabled'))
+					textbox.classList.add('focused');
 
-	window.addEventListener('click', (e) => {
-		if(!(e.target.classList.contains('btn') && e.target.parentNode.classList.contains('btn-piano') && e.target.parentNode.parentNode.classList.contains('dropdown'))){
-			__dropdowns.forEach((__dropdown) => {
-				let
-					__trigger = __dropdown.querySelector('.btn'),
-					__dropable = __dropdown.querySelector('div.btn-piano');
-		
-				if(__dropable.style.display === 'inline' && e.target.parentNode !== __dropdown) {
-					__dropable.style.display = 'none';
-					__trigger.classList.remove('dropped');
-				}
+				if (!textbox.classList.contains('placeholder-off') && !textbox.classList.contains('disabled'))
+					textbox.classList.add('placeholder-off');
 			});
-		}
-	});
+
+			input.addEventListener('blur', () => {
+				if (textbox.classList.contains('focused'))
+					textbox.classList.remove('focused');
+
+				if (textbox.classList.contains('placeholder-off') && input.value.length === 0)
+					textbox.classList.remove('placeholder-off');
+			});
+
+			// Trigger the focus event.
+			if (input.value.length > 0)
+				input.dispatchEvent(new Event('focus'));
+		});
+	})();
 });
