1ad6275e352a1f845512227e498b751f79506154
Eoussama <oussama_gtacj@live.fr>
2018-11-17 15:20:31 +0000

    Radio buttons added.

--- src/dist/js/eomponents.js
+++ src/dist/js/eomponents.js
@@ -38,9 +38,17 @@ window.addEventListener('load', () => {
 					textbox.classList.remove('placeholder-off');
 			});
 
+			input.addEventListener('change', () => {
+				textbox.value = input.value;
+			});
+
 			// Trigger the focus event.
-			if (input.value.length > 0)
+			if (input.value.length > 0) {
 				input.focus();
+				textbox.value = input.value;
+			} else {
+				textbox.value = '';
+			}
 		});
 	})();
 
@@ -50,7 +58,7 @@ window.addEventListener('load', () => {
 
 		checkboxes.forEach(checkbox => {
 			const checkboxInput = checkbox.children[0];
-			
+
 			checkbox.addEventListener('click', () => {
 				if (!checkboxInput['disabled'] && !checkbox.classList.contains('disabled')) {
 					if (checkboxInput.checked) {
@@ -60,11 +68,45 @@ window.addEventListener('load', () => {
 						checkbox.classList.add('checked');
 						checkboxInput.checked = true;
 					}
+
+					checkbox.value = checkboxInput.checked;
 				}
 			});
 
-			if (checkboxInput.checked)
+			if (checkboxInput.checked) {
 				checkbox.classList.add('checked');
+				checkbox.value = true;
+			} else {
+				checkbox.value = false;
+			}
+		});
+	})();
+
+	// Radio
+	(function() {
+		const radios = document.querySelectorAll('div.radio');
+
+		radios.forEach(radio => {
+			const radioInput = radio.children[0];
+
+			radio.addEventListener('click', () => {
+				if (radioInput.checked) {
+					radio.classList.remove('checked');
+					radioInput.checked = false;
+				} else {
+					radio.classList.add('checked');
+					radioInput.checked = true;
+				}
+
+				radio.value = radioInput.checked;
+			});
+
+			if (radioInput.checked) {
+				radio.classList.add('checked');
+				radio.value = true;
+			} else {
+				radio.value = false;
+			}
 		});
 	})();
 });
